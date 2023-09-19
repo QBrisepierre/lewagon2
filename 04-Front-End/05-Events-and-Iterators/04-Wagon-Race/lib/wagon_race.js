1a9003eb447f9386744193b1819152fa1cd496b4
Quentin Brisepierre <Quentin.brisepierre@protonmail.com>
2023-09-19 15:18:45 +0200

    done

--- 04-Front-End/05-Events-and-Iterators/04-Wagon-Race/lib/wagon_race.js
+++ 04-Front-End/05-Events-and-Iterators/04-Wagon-Race/lib/wagon_race.js
@@ -1,1 +1,22 @@
 // TODO: write your code here
+const moveForward = (player) => {
+  const player1 = document.querySelector(`#player${player}-race .active`);
+  if (player1.nextElementSibling) {
+    player1.nextElementSibling.classList.add("active");
+    player1.classList.remove("active");
+  } else {
+    alert(`Player1 wins`);
+    window.location.reload();
+  }
+};
+
+
+const moveWagons = (event) => {
+  if (event.key === "q") {
+    moveForward(1);
+  } else if (event.key === "p") {
+    moveForward(2);
+  }
+};
+
+document.addEventListener("keyup", moveWagons);
