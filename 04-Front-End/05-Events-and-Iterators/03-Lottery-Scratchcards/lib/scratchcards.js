ad3b7934b32ae6c0c0db0f22b67e59c226a4fa54
Quentin Brisepierre <Quentin.brisepierre@protonmail.com>
2023-09-19 15:01:27 +0200

    done

--- 04-Front-End/05-Events-and-Iterators/03-Lottery-Scratchcards/lib/scratchcards.js
+++ 04-Front-End/05-Events-and-Iterators/03-Lottery-Scratchcards/lib/scratchcards.js
@@ -1,1 +1,28 @@
 // TODO: Write your gambling application here.
+const balance = document.getElementById("balance");
+let count = Number(document.getElementById("balance").innerHTML);
+
+function updateBalance(ticket) {
+  count -= 10;
+  count += Number(ticket.dataset.amount);
+  balance.innerHTML = count;
+}
+
+function updateTicket(ticket) {
+  ticket.dataset.scratched = true;
+  ticket.innerHTML = ticket.dataset.amount;
+}
+
+document.querySelectorAll('li').forEach((ticket) => {
+  ticket.addEventListener("click", (event) => {
+    if (balance.innerHTML > 0) {
+      console.log(ticket.dataset.scratched);
+      if (ticket.dataset.scratched === "false") {
+        updateBalance(ticket);
+        updateTicket(ticket);
+      }
+    } else {
+      alert("Game Over");
+    }
+  });
+});
