b6e5f63e82f71453103028c3b88adc48c3cce6b5
Quentin Brisepierre <Quentin.brisepierre@protonmail.com>
2023-09-19 12:12:14 +0200

    done

--- 04-Front-End/05-Events-and-Iterators/02-My-First-Event-Listener/lib/listener.js
+++ 04-Front-End/05-Events-and-Iterators/02-My-First-Event-Listener/lib/listener.js
@@ -1,1 +1,6 @@
 // TODO: React to a click on the button!
+const button = document.getElementById('clickme');
+const audio = new Audio('sound.mp3');
+button.addEventListener("click", (event) => {
+  audio.play();
+});
