e9b29d3264b005fe76ad5ef6c5bd7ccccd3c9beb
Quentin Brisepierre <Quentin.brisepierre@protonmail.com>
2023-09-19 11:23:14 +0200

    done

--- 04-Front-End/04-JS-and-DOM/04-Fake-Inbox/lib/inbox.js
+++ 04-Front-End/04-JS-and-DOM/04-Fake-Inbox/lib/inbox.js
@@ -6,19 +6,34 @@ import runChallenges from "../spec/inbox_examiner.js";
 
 const hasNewMessage = () => {
   // TODO: return true with a probability of 20%.
+  return Math.random() < 0.2;
 };
 
+console.log(hasNewMessage);
 const newMessage = () => {
   // TODO: return a random message as an object with two keys, subject and sender
+  return {
+    sender: `Github ${Math.random()}`,
+    subject: `Github ${Math.random()}`
+  };
 };
 
 const appendMessageToDom = (message) => {
   // TODO: append the given message to the DOM (as a new row of `#inbox`)
+  const myMessage = `<div class="row message unread">
+    <div class="col-3">${message.sender}</div>
+    <div class="col-9">${message.subject}</div>
+    </div>`;
+  const inbox = document.getElementById("inbox");
+  inbox.insertAdjacentHTML('afterbegin', myMessage);
 };
 
 const refresh = () => {
   // TODO: Implement the global refresh logic. If there is a new message,
   //       append it to the DOM. Update the unread counter in title as well.
+  if (hasNewMessage()) {
+    appendMessageToDom(newMessage());
+  }
 };
 
 // Do not remove these lines.
