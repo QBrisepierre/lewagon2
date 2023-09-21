b48000f991476bb37c9c9ae8285ccf25701aec51
Quentin Brisepierre <Quentin.brisepierre@protonmail.com>
2023-09-21 10:54:34 +0200

    class active toggle

--- 04-Front-End/06-HTTP-and-API/02-My-Favourite-Sports/lib/index.js
+++ 04-Front-End/06-HTTP-and-API/02-My-Favourite-Sports/lib/index.js
@@ -1,1 +1,13 @@
 // TODO: write your code here!
+const sportsList = document.querySelectorAll(".clickable");
+
+const displayClickedElement = (event) => {
+  event.currentTarget.classList.toggle("active");
+  // sport.classList.toggle("active");
+};
+
+const bindButtonToClick = (button) => {
+  button.addEventListener("click", displayClickedElement);
+};
+
+sportsList.forEach(bindButtonToClick);
