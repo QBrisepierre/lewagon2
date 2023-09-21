413b65c18be9590bf5572e47bfb1d7d43d5dd35c
Quentin Brisepierre <Quentin.brisepierre@protonmail.com>
2023-09-21 14:41:51 +0200

    refoactoring the code

--- 04-Front-End/06-HTTP-and-API/04-The-Fork-Restaurants/lib/index.js
+++ 04-Front-End/06-HTTP-and-API/04-The-Fork-Restaurants/lib/index.js
@@ -1,28 +1,38 @@
 // eslint-disable-next-line max-len
 // Todo: Use the Fork Api to get a filtered list of restaurants, depending on the category selected (and as a bonus, the location)
-
+const apiUrl = "https://the-fork-api.students.lewagon.co/api/v1/restaurants";
 const searchForm = document.getElementById("searchForm");
+const categoriesRadioButtons = document.querySelectorAll(".form-check-input");
+const locationInput = document.getElementById("location");
+const resultsList = document.getElementById("restaurant-list");
 
 // Todo: select the HTML elements you need
 
-const insertResults = (restaurant) => {
-  const restaurantHtml = `<li class="list-group-item">${restaurant.name}</li>`;
-  const list = document.getElementById("restaurant-list");
-  list.insertAdjacentHTML("beforeend", restaurantHtml);
+const insertResults = (results) => {
+  resultsList.innerHTML = "";
+
+  if (results.length > 0) {
+    results.forEach((restaurant) => {
+      resultsList.insertAdjacentHTML("beforeend", `<li class="list-group-item">${restaurant.name} in ${restaurant.address}</li>`);
+    });
+  } else {
+    resultsList.innerHTML = "<p class=\"text-muted text-center\">No restaurants found! Please select another category or location.</p>";
+  }
 };
 
-const buildSearchUrl = (event) => {
-  const value = Array.from(document.querySelectorAll('input[type=radio]:checked'))
-    .map(item => item.value)
-    .join(',');
-  const location = event.target[9].value;
-  return `https://the-fork-api.students.lewagon.co/api/v1/restaurants?category=${value}&location=${location}`;
+const buildSearchUrl = () => {
+  const checkedCategory = Array.from(categoriesRadioButtons).find(element => element.checked);
+  const categoryValue = checkedCategory.value;
+  const locationValue = locationInput.value;
+
+  return `${apiUrl}?category=${categoryValue}&location=${locationValue}`;
 };
 
 
 searchForm.addEventListener("submit", (event) => {
   // Todo: Find the category selected and build the URL you will send your request to
   event.preventDefault();
+
   const url = buildSearchUrl(event);
 
   // Todo: Replace "the-endpoint-url" with the URL you built
@@ -30,9 +40,6 @@ searchForm.addEventListener("submit", (event) => {
     .then(response => response.json())
     .then((data) => {
       // Todo: Insert results into the list
-      console.log(data);
-      data.forEach((restaurant) => {
-        insertResults(restaurant);
-      });
+      insertResults(data);
     });
 });
