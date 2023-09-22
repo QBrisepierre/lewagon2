5cf2063b2ae1be24f46ccebd9101b7e694bd1d3b
Quentin Brisepierre <Quentin.brisepierre@protonmail.com>
2023-09-22 15:05:03 +0200

    chat done post and refresh

--- 04-Front-End/07-AJAX/03-Anonymous-Chat/lib/index.js
+++ 04-Front-End/07-AJAX/03-Anonymous-Chat/lib/index.js
@@ -1,4 +1,49 @@
-const batch = 1; // change to your own batch id
+const batch = 401; // change to your own batch id
 const baseUrl = "https://wagon-chat.herokuapp.com/";
 
 // Your turn to code!
+const url = `${baseUrl}${batch}/messages`;
+const refresh = document.getElementById("refresh");
+const send = document.querySelector(".btn-primary");
+const lists = document.querySelector(".list-unstyled");
+const comment = document.getElementById("your-message");
+const name = document.getElementById("your-name");
+
+const refreshMessages = () => {
+  fetch(url)
+    .then(response => response.json())
+    .then((data) => {
+      lists.innerHTML = "";
+      const messages = data.messages;
+      console.log(messages);
+      messages.forEach((message) => {
+        const minutesAgo = Math.round((new Date() - new Date(message.created_at)) / 60000);
+        lists.insertAdjacentHTML("afterbegin", `<li>${message.content} (posted <span class="date">${minutesAgo} minutes ago</span>) by ${message.author}</li>`);
+      });
+    });
+};
+
+const postNewMessage = (message, callback) => {
+  fetch(url, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(message)
+  })
+    .then(response => response.json())
+    .then((data) => {
+      console.log(data);
+      callback();
+    });
+};
+
+send.addEventListener("click", (event) => {
+  event.preventDefault();
+  const data = { "author": name.value, "content": comment.value };
+  postNewMessage(data, refreshMessages);
+});
+
+refresh.addEventListener("click", refreshMessages);
+
+document.addEventListener("DOMContentLoaded", refreshMessages);
+
+setInterval(refreshMessages, 5000);
