410a602868fdc2a715f265d1fa30729f03d44b21
Quentin Brisepierre <Quentin.brisepierre@protonmail.com>
2023-09-22 13:23:25 +0200

    done

--- 04-Front-End/07-AJAX/01-Name-Age/lib/index.js
+++ 04-Front-End/07-AJAX/01-Name-Age/lib/index.js
@@ -1,11 +1,18 @@
-const agifyApiUrl = "https://api.agify.io/"
+const agifyApiUrl = "https://api.agify.io/";
 
 const displayAge = (event) => {
   event.preventDefault();
   const firstName = document.getElementById("first-name").value;
+  const age = document.getElementById("your-age");
   // TODO: Display your age with an AJAX call instead of the console.log()
   console.log(firstName);
-}
+  const url = `${agifyApiUrl}?name=${firstName}`;
+  fetch(url)
+    .then(response => response.json())
+    .then((data) => {
+      age.innerText = `You're ${data.age} years old`;
+    });
+};
 
 const form = document.getElementById("fetch-age");
 form.addEventListener("submit", displayAge);
