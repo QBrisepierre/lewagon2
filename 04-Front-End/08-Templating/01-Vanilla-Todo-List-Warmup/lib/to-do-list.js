ca0a1027a364c4548ac42e4a0ac83dae95dc82fe
Quentin Brisepierre <Quentin.brisepierre@protonmail.com>
2023-09-25 11:40:58 +0200

    todo done

--- 04-Front-End/08-Templating/01-Vanilla-Todo-List-Warmup/lib/to-do-list.js
+++ 04-Front-End/08-Templating/01-Vanilla-Todo-List-Warmup/lib/to-do-list.js
@@ -5,6 +5,16 @@ const todos = [
   { title: "Water the plants", done: true }
 ];
 
+const todosContainer = document.getElementById("todosContainer");
+todos.forEach((todo) => {
+  const task = `<div class="shadow-sm rounded px-4 py-3 mb-2 border d-flex">
+     <input class="d-flex form-check-input me-1" type="checkbox" ${todo.done ? "checked" : ""}>
+     <div>${todo.title}
+     </div>
+   </div>`;
+
+  todosContainer.insertAdjacentHTML("beforeend", task);
+});
 // To-do item HTML 👇
 // <div class="shadow-sm rounded px-4 py-3 mb-2 border d-flex">
 //   <input class="d-flex form-check-input me-1" type="checkbox">
