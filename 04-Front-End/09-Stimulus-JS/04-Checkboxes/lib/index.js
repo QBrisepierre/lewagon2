54c4b3389dba1637a9fe381d661e73d45ace3aeb
Quentin Brisepierre <Quentin.brisepierre@protonmail.com>
2023-10-05 10:19:12 +0200

    done

--- 04-Front-End/09-Stimulus-JS/04-Checkboxes/lib/index.js
+++ 04-Front-End/09-Stimulus-JS/04-Checkboxes/lib/index.js
@@ -1,6 +1,8 @@
 import { Application } from "@hotwired/stimulus";
 
 // TODO: Import the Stimulus Controller here
+import checkAllCheckboxesController from "./controllers/check_all_checkboxes_controller.js";
 
 window.Stimulus = Application.start();
 // TODO: Register your Stimulus Controller below
+Stimulus.register("check-all-checkboxes", checkAllCheckboxesController);
